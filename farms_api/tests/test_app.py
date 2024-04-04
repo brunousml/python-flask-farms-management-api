a61fa75efed38134a718747aa5c123845171744c
Bruno Melo <bruno.melo@jusbrasil.com.br>
2024-04-03 21:58:40 -0300

    Update test_app.py

--- farms_api/tests/test_app.py
+++ farms_api/tests/test_app.py
@@ -1,4 +1,4 @@
 def test_should_return_hello_farmer_when_get_root_path(client):
     response = client.get("/")
 
-    assert b'Hello, Farmer!' in response.data
+    assert b'Hello, Farmer test!' in response.data
